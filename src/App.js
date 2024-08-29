12efe8e39c3163901d0bed4b6a53810eb5f7c79b
Rudra Dey Sarkar <97943164+Rudra-Dey-Sarkar@users.noreply.github.com>
2024-08-29 20:05:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { PlatformProvider } from './components/Global Data/PlatformContext';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import Navbar from './components/Navbar/Navbar';
-import Homepage from './components/Homepage/Homepage';
-import Footer from './components/Footer/Footer';
-import Login from './components/Login/Login';
-import SignUp from './components/Signup/Signup'
-import User from './components/User/User';
+
 
 function App() {
   return (
