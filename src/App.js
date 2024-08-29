7ad63d943572839fb286334c9b9cf5438a773839
Rudra Dey Sarkar <97943164+Rudra-Dey-Sarkar@users.noreply.github.com>
2024-08-29 20:02:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { PlatformProvider } from './components/Global Data/PlatformContext';
 function App() {
   return (
     <div className="App">
