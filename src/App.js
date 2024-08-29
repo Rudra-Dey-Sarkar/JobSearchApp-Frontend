0f7e5368fe972b17f869ecf97e83ed4407dd66bf
Rudra Dey Sarkar <97943164+Rudra-Dey-Sarkar@users.noreply.github.com>
2024-08-29 19:52:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+//import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from './components/Navbar/Navbar';
@@ -10,54 +10,54 @@ import SignUp from './components/Signup/Signup'
 import User from './components/User/User';
 import { PlatformProvider } from './components/Global Data/PlatformContext';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <>
-        <Navbar />
-        <Homepage />
-        <Footer />
-      </>
-    ),
-  },
-  {
-    path: "/login",
-    element: (
-      <>
-        <Navbar />
-        <Login />
-        <Footer />
-      </>
-    ),
-  },
-  {
-    path: "/signup",
-    element: (
-      <>
-        <Navbar />
-        <SignUp />
-        <Footer />
-      </>
-    ),
-  },
-  {
-    path: "/user",
-    element: (
-      <>
-        <Navbar />
-        <User />
-        <Footer />
-      </>
-    ),
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: (
+//       <>
+//         <Navbar />
+//         <Homepage />
+//         <Footer />
+//       </>
+//     ),
+//   },
+//   {
+//     path: "/login",
+//     element: (
+//       <>
+//         <Navbar />
+//         <Login />
+//         <Footer />
+//       </>
+//     ),
+//   },
+//   {
+//     path: "/signup",
+//     element: (
+//       <>
+//         <Navbar />
+//         <SignUp />
+//         <Footer />
+//       </>
+//     ),
+//   },
+//   {
+//     path: "/user",
+//     element: (
+//       <>
+//         <Navbar />
+//         <User />
+//         <Footer />
+//       </>
+//     ),
+//   },
+// ]);
 
 function App() {
   return (
     <PlatformProvider>
     <div className="App">
-      <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
     </div>
     </PlatformProvider>
   );
