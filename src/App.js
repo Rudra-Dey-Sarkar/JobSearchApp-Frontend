b5c6a722065666c69cfc245a36273342b703f326
Rudra Dey Sarkar <97943164+Rudra-Dey-Sarkar@users.noreply.github.com>
2024-08-29 20:00:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,65 +1,11 @@
 import React from 'react';
-//import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import Navbar from './components/Navbar/Navbar';
-import Homepage from './components/Homepage/Homepage';
-import Footer from './components/Footer/Footer';
-import Login from './components/Login/Login';
-import SignUp from './components/Signup/Signup'
-import User from './components/User/User';
-//import { PlatformProvider } from './components/Global Data/PlatformContext';
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: (
-//       <>
-//         <Navbar />
-//         <Homepage />
-//         <Footer />
-//       </>
-//     ),
-//   },
-//   {
-//     path: "/login",
-//     element: (
-//       <>
-//         <Navbar />
-//         <Login />
-//         <Footer />
-//       </>
-//     ),
-//   },
-//   {
-//     path: "/signup",
-//     element: (
-//       <>
-//         <Navbar />
-//         <SignUp />
-//         <Footer />
-//       </>
-//     ),
-//   },
-//   {
-//     path: "/user",
-//     element: (
-//       <>
-//         <Navbar />
-//         <User />
-//         <Footer />
-//       </>
-//     ),
-//   },
-// ]);
 
 function App() {
   return (
-  {/*  <PlatformProvider> */}
     <div className="App">
-    {/* <RouterProvider router={router} /> */}
+   Test
     </div>
-   {/*  </PlatformProvider> */}
+
   );
 }
 
