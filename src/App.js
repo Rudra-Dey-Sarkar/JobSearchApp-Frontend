fda457760e9b107ba8693ee6ba3b15ad824ceac6
Rudra Dey Sarkar <97943164+Rudra-Dey-Sarkar@users.noreply.github.com>
2024-08-29 20:08:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { PlatformProvider } from './components/Global Data/PlatformContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
